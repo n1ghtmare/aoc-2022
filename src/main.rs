44e1d07179aee06d826cb0137ebda91c4dc1fe15
Dimitar Dimitrov <dimitrov2k@gmail.com>
2022-12-08 20:08:31 +0200

    day 2, refactoring of part 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
-    // day_1();
-
-    day_2_part_2();
+    day_2_part_1();
 }
 
 /*
@@ -12,9 +10,11 @@ Rules:
 
 Scores:
 -
-Rock: 1
-Paper: 2
-Scissors: 3
+             O   P
+             _____
+Rock        (A | X): 1
+Paper       (B | Y): 2
+Scissors    (C | Z): 3
 
 Outcomes:
 -
@@ -22,37 +22,76 @@ Loss: 0
 Draw: 3
 Win: 6
 
+Winning combinations (for P):
+CX, 1
+AY, 2
+BZ, 3
+
+Losing combinations (for P):
+AZ, 3
+BX, 1
+CY, 2
+
+Draw combinations:
+AX, 1
+BY, 2
+CZ, 3
 */
 
-#[allow(dead_code)]
-fn day_2_part_1() {
-    let shapes: HashMap<&str, u32> =
-        HashMap::from([("A", 1), ("X", 1), ("B", 2), ("Y", 2), ("C", 3), ("Z", 3)]);
+enum Outcome {
+    Win,
+    Loss,
+    Draw,
+}
 
-    let mut score: u32 = 0;
+enum Sign {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn get_score(sign: Sign, outcome: Outcome) -> usize {
+    let sign: usize = match sign {
+        Sign::Rock => 1,
+        Sign::Paper => 2,
+        Sign::Scissors => 3,
+    };
+
+    let outcome: usize = match outcome {
+        Outcome::Win => 6,
+        Outcome::Loss => 0,
+        Outcome::Draw => 3,
+    };
+
+    sign + outcome
+}
+
+fn day_2_part_1() {
+    let outcome_combinations: HashMap<&str, usize> = HashMap::from([
+        ("CX", get_score(Sign::Rock, Outcome::Win)),
+        ("AY", get_score(Sign::Paper, Outcome::Win)),
+        ("BZ", get_score(Sign::Scissors, Outcome::Win)),
+        ("AZ", get_score(Sign::Scissors, Outcome::Loss)),
+        ("BX", get_score(Sign::Rock, Outcome::Loss)),
+        ("CY", get_score(Sign::Paper, Outcome::Loss)),
+        ("AX", get_score(Sign::Rock, Outcome::Draw)),
+        ("BY", get_score(Sign::Paper, Outcome::Draw)),
+        ("CZ", get_score(Sign::Scissors, Outcome::Draw)),
+    ]);
 
     let file_data = include_str!("./input/day2/input_prod.txt");
 
-    for line in file_data.lines() {
-        let round_shapes = line.split(" ").collect::<Vec<&str>>();
-
-        let o = shapes.get(round_shapes[0]).unwrap().clone();
-        let p = shapes.get(round_shapes[1]).unwrap().clone();
-
-        let round_outcome: u32 = if (p == 3 && o == 2) || (p == 2 && o == 1) || (p == 1 && o == 3) {
-            6
-        } else if p == o {
-            3
-        } else {
-            0
-        };
-
-        score = score + round_outcome + p;
-    }
+    let score: usize = file_data
+        .lines()
+        .map(
+            |x| match outcome_combinations.get(x.replace(" ", "").as_str()) {
+                Some(o) => o.clone(),
+                None => 0,
+            },
+        )
+        .sum();
 
     println!("score: {}", score);
-
-    // dbg!("{}", lines);
 }
 
 /*
@@ -66,6 +105,8 @@ X: Loose = 0
 Y: Draw = 3
 Z: Win = 6
 */
+
+#[allow(dead_code)]
 fn day_2_part_2() {
     let shapes: HashMap<&str, u32> = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
 
