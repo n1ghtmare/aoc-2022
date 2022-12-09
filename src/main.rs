66334880d8cdd64a1bf2f376f4a048910f76df8e
Dimitar Dimitrov <dimitrov2k@gmail.com>
2022-12-09 20:25:42 +0200

    day 2, refactoring of part 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
-    day_2_part_1();
+    day_2_part_2();
 }
 
 /*
@@ -50,14 +50,14 @@ enum Sign {
     Scissors,
 }
 
-fn get_score(sign: Sign, outcome: Outcome) -> usize {
-    let sign: usize = match sign {
+fn get_score(sign: Sign, outcome: Outcome) -> u8 {
+    let sign: u8 = match sign {
         Sign::Rock => 1,
         Sign::Paper => 2,
         Sign::Scissors => 3,
     };
 
-    let outcome: usize = match outcome {
+    let outcome: u8 = match outcome {
         Outcome::Win => 6,
         Outcome::Loss => 0,
         Outcome::Draw => 3,
@@ -66,26 +66,29 @@ fn get_score(sign: Sign, outcome: Outcome) -> usize {
     sign + outcome
 }
 
+fn get_score_by_combination(combination: &str) -> Option<u8> {
+    match combination {
+        "CX" => Some(get_score(Sign::Rock, Outcome::Win)),
+        "AY" => Some(get_score(Sign::Paper, Outcome::Win)),
+        "BZ" => Some(get_score(Sign::Scissors, Outcome::Win)),
+        "AZ" => Some(get_score(Sign::Scissors, Outcome::Loss)),
+        "BX" => Some(get_score(Sign::Rock, Outcome::Loss)),
+        "CY" => Some(get_score(Sign::Paper, Outcome::Loss)),
+        "AX" => Some(get_score(Sign::Rock, Outcome::Draw)),
+        "BY" => Some(get_score(Sign::Paper, Outcome::Draw)),
+        "CZ" => Some(get_score(Sign::Scissors, Outcome::Draw)),
+        _ => None,
+    }
+}
+
+#[allow(dead_code)]
 fn day_2_part_1() {
-    let outcome_combinations: HashMap<&str, usize> = HashMap::from([
-        ("CX", get_score(Sign::Rock, Outcome::Win)),
-        ("AY", get_score(Sign::Paper, Outcome::Win)),
-        ("BZ", get_score(Sign::Scissors, Outcome::Win)),
-        ("AZ", get_score(Sign::Scissors, Outcome::Loss)),
-        ("BX", get_score(Sign::Rock, Outcome::Loss)),
-        ("CY", get_score(Sign::Paper, Outcome::Loss)),
-        ("AX", get_score(Sign::Rock, Outcome::Draw)),
-        ("BY", get_score(Sign::Paper, Outcome::Draw)),
-        ("CZ", get_score(Sign::Scissors, Outcome::Draw)),
-    ]);
-
     let file_data = include_str!("./input/day2/input_prod.txt");
-
-    let score: usize = file_data
+    let score: u32 = file_data
         .lines()
         .map(
-            |x| match outcome_combinations.get(x.replace(" ", "").as_str()) {
-                Some(o) => o.clone(),
+            |x| match get_score_by_combination(x.replace(" ", "").as_str()) {
+                Some(o) => o as u32,
                 None => 0,
             },
         )
@@ -106,48 +109,51 @@ Y: Draw = 3
 Z: Win = 6
 */
 
-#[allow(dead_code)]
+fn get_score_for_round(expected_outcome: &str, opponent_shape: &str) -> u32 {
+    match expected_outcome {
+        // Loss
+        "X" => match opponent_shape {
+            "A" => 3,
+            "B" => 1,
+            "C" => 2,
+            _ => 0,
+        },
+        "Z" => {
+            6 + match opponent_shape {
+                "A" => 2,
+                "B" => 3,
+                "C" => 1,
+                _ => 0,
+            }
+        }
+        "Y" => {
+            3 + match opponent_shape {
+                "A" => 1,
+                "B" => 2,
+                "C" => 3,
+                _ => 0,
+            }
+        }
+        _ => 0,
+    }
+}
+
 fn day_2_part_2() {
-    let shapes: HashMap<&str, u32> = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
-
-    let mut score: u32 = 0;
-
     let file_data = include_str!("./input/day2/input_prod.txt");
 
-    for line in file_data.lines() {
-        let columns = line.split(" ").collect::<Vec<&str>>();
-
-        let o = shapes.get(columns[0]).unwrap().clone();
-
-        let round_outcome = match columns[1] {
-            "X" => match o {
-                3 => 2,
-                2 => 1,
-                1 => 3,
-                _ => 0,
-            },
-            "Y" => 3 + o,
-            "Z" => {
-                6 + match o {
-                    3 => 1,
-                    2 => 3,
-                    1 => 2,
-                    _ => 0,
-                }
-            }
-            _ => 0,
-        };
-
-        score = score + round_outcome;
-    }
+    let score: u32 = file_data
+        .lines()
+        .map(|x| {
+            let (opponent_shape, expected_outcome) = x.split_once(" ").unwrap();
+            get_score_for_round(expected_outcome, opponent_shape)
+        })
+        .sum();
 
     println!("score: {}", score);
-
-    // dbg!("{}", lines);
 }
 
 #[allow(dead_code)]
-fn day_1() {
+fn day_1_final() {
     let file_data = include_str!("./input/day1/input_prod.txt");
 
     let mut calories: Vec<u32> = file_data
